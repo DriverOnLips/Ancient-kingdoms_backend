bcd6ebb033caafac48f9ce0aa40cbb14bdf89879
Roman Sulaymanov <92111647+Romanlock@users.noreply.github.com>
2023-11-09 11:55:58 +0300

    add server base

---
+++ cmd/migrate/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"kingdoms/internal/app/connect"
+	"kingdoms/internal/app/database"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	_ = godotenv.Load()
+	db, err := gorm.Open(postgres.Open(connect.FromEnv()), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Failed to connect database! Error:", err)
+		return
+	}
+
+	// Migrate the schema
+	err = MigrateSchema(db)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func MigrateSchema(db *gorm.DB) error {
+	err := MigrateKingdom(db)
+	if err != nil {
+		return err
+	}
+
+	err = MigrateUser(db)
+	if err != nil {
+		return err
+	}
+
+	err = MigrateRuler(db)
+	if err != nil {
+		return err
+	}
+
+	err = MigrateRuling(db)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func MigrateKingdom(db *gorm.DB) error {
+	err := db.AutoMigrate(&database.Kingdom{})
+	if err != nil {
+		fmt.Println("Error migrating Kingdom to db")
+		return err
+	}
+
+	return nil
+}
+
+func MigrateUser(db *gorm.DB) error {
+	err := db.AutoMigrate(&database.User{})
+	if err != nil {
+		fmt.Println("Error migrating User to db")
+		return err
+	}
+
+	return nil
+}
+
+func MigrateRuler(db *gorm.DB) error {
+	err := db.AutoMigrate(&database.Ruler{})
+	if err != nil {
+		fmt.Println("Error migrating Ruler to db")
+		return err
+	}
+
+	return nil
+}
+
+func MigrateRuling(db *gorm.DB) error {
+	err := db.AutoMigrate(&database.Ruling{})
+	if err != nil {
+		fmt.Println("Error migrating Ruling to db")
+		return err
+	}
+
+	return nil
+}
